708e10fe1bf56010703e04ffe292e307b36619cf
msalves008 <matheusdesousaalves2018@gmail.com>
2021-12-11 18:10:24 -0300

    make the Sidebar Responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
-import { Flex, useBreakpoint, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, useBreakpoint, useBreakpointValue, IconButton } from "@chakra-ui/react";
 import { Profile } from "./Profile";
 import SearchBox from "./SearchBox";
 import NotificationsNav from "./NotificationsNav";
 import Logo from "./Logo";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+
+import { RiMenuLine } from "react-icons/ri";
 
 export default function Header() {
+
+  const {onOpen} = useSidebarDrawer()
+
   const isWideVersion =  useBreakpointValue({
     base: false,
     lg: true,
@@ -20,6 +26,17 @@ export default function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+        aria-label="Open sidebar"
+        icon={<Icon as={RiMenuLine}/>}
+        fontSize="24"
+        variant="unstyled"
+        onClick={onOpen}
+        mr='2'>
+          
+        </IconButton>
+      )}
       <Logo />
     {isWideVersion && <SearchBox />}
       <Flex align="center" ml="auto">
