6164b5cc831ff9ca023006a7e9f77749f86131b0
msalves008 <matheusdesousaalves2018@gmail.com>
2021-12-18 06:33:53 -0300

    make Prefeth of user data on Mouse hover top user name

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,26 +14,35 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from "@chakra-ui/react";
 
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Pagination from "../../components/Pagination";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
-
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
   const { data, isLoading, isFetching, error } = useUsers(page);
-  
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+  async function handlePrefethUser(userId: string){
+    await queryClient.prefetchQuery(['user', userId], async ()=>{
+      const response = await api.get(`users/${userId}`);
+      return response.data;
+    },{
+      staleTime: 1000 * 60 *10, // 10 minutes
+    })   
+  }
 
   return (
     <Box>
@@ -45,9 +54,11 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" alignI="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
-              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml='4' />}
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -57,7 +68,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           {isLoading ? (
             <Flex justify="center">
@@ -89,7 +100,9 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link color='purple.400' onMouseEnter={()=> handlePrefethUser(user.id)}>
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">
                               {user.email}
                             </Text>
@@ -101,7 +114,7 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination 
+              <Pagination
                 totalCountOfRegisters={data.totalCount}
                 currentPage={page}
                 onPageChange={setPage}
